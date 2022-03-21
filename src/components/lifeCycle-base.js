dfea0de6bc6c9d8cbd5d584ba06c5ef3f048a0e4
boyan <baiyan@souche.com>
2022-03-22 00:19:10 +0800

    feat: init

---
+++ src/components/lifeCycle-base.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0,
+    };
+    console.log("1 setup state and props");
+  }
+
+  componentWillMount() {
+    console.log("2 componentWillMount");
+  }
+  componentDidMount() {
+    console.log("4 componentDidMount");
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("5 shouldComponentUpdate");
+    return nextState.num % 2 === 0;
+  }
+  componentWillUpdate() {
+    console.log("6 componentWillUpdate");
+  }
+  componentDidUpdate() {
+    console.log("7 componentWillDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("8 componentWillUnMount");
+  }
+
+  add = () => {
+    this.setState({ num: this.state.num + 1 });
+  };
+
+  render() {
+    console.log("3 render");
+    return (
+      <div>
+        {this.state.num}
+        <button onClick={this.add}>add</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import ReactDOM from "./react-dom";
+import ReactDOM from "react-dom";
 // import Welcome from "./components/class-component";
 // import Tick from "./components/setState";
-import Sum from "./components/createRef";
+// import Sum from "./components/createRef";
+import Counter from "./components/lifeCycle-base";
 
 const root = document.getElementById("root");
 ReactDOM.render(
   // <Welcome className="title" style={{ color: "purple" }} name="打工人！" />,
   // <Tick />,
-  <Sum />,
+  // <Sum />,
+  <Counter />,
   root
 );
